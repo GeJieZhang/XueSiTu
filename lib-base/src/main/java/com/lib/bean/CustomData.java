798d72b3db2a7335a8964e08079465d960545e51
PC-201903292241\Administrator <1241577948@qq.com>
2019-05-28 17:50:55 +0800

    2019/5/28

--- lib-base/src/main/java/com/lib/bean/CustomData.java
+++ lib-base/src/main/java/com/lib/bean/CustomData.java
@@ -1,4 +1,4 @@
-package com.youxuan.bean;
+package com.dayi.bean;
 
 /**
  * Created by songwenchao
---
+++ module-dayi/src/main/java/com/dayi/fragment/banner/CustomViewHolder2.java
@@ -0,0 +1,43 @@
+package com.youxuan.fragment.banner;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.lib.fastkit.utils.log.LogUtil;
+import com.ms.banner.holder.BannerViewHolder;
+
+import com.youxuan.R;
+import com.youxuan.bean.CustomData;
+
+/**
+ * Created by songwenchao
+ * on 2018/5/17 0017.
+ * <p>
+ * 类名
+ * 需要 --
+ * 可以 --
+ */
+public class CustomViewHolder2 implements BannerViewHolder<CustomData> {
+
+
+    private ImageView image1;
+
+    @Override
+    public View createView(Context context) {
+        View view = LayoutInflater.from(context).inflate(R.layout.banner_item2, null);
+        image1 = view.findViewById(R.id.image1);
+
+
+        return view;
+    }
+
+    @Override
+    public void onBind(Context context, int position, CustomData data) {
+        image1.setImageResource(R.mipmap.banner_1);
+
+
+       // LogUtil.e(data.getName());
+    }
+}
